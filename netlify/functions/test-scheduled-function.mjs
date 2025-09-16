e3624154404b74e9711c72f7f104aedc01c6eba7
brodrigues0ll <bernardorg99@hotmail.com>
2025-09-16 12:36:30 -0300

    Tryin cronjob

--- netlify/functions/test-scheduled-function.mjs
+++ netlify/functions/test-scheduled-function.mjs
@@ -1,10 +1,19 @@
 export default async (req) => {
   const { next_run } = await req.json();
 
-  await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/reports/update`);
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ next_run }),
+  const update = async () => {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/reports/update`,
+      {
+        method: "GET",
+      }
+    );
+    const data = await res.json();
+    console.log("✅ Relatórios atualizados:", data.length);
   };
+
+  console.log(
+    "⏰ Executando a próxima execução de atualização de relatórios..."
+  );
+  await update();
 };
