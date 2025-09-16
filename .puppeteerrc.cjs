689b8b4ebb5316d531c8e74e30494575f1df0911
brodrigues0ll <bernardorg99@hotmail.com>
2025-09-16 08:51:23 -0300

    netlify

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,15 @@
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // Download Chrome (default `skipDownload: false`).
+  chrome: {
+    skipDownload: false,
+  },
+  // Download Firefox (default `skipDownload: true`).
+  firefox: {
+    skipDownload: true,
+  },
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+};
